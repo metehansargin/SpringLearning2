ce33d31d4ad0b5abe7ab9b869d3f75ecd1d9a96f
metehansargin <msargin1986@gmail.com>
2026-02-17 19:33:02 +0300

    first commit

---
+++ src/test/java/com/metehansargin/springlearn2/Springlearn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metehansargin.springlearn2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlearn2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
