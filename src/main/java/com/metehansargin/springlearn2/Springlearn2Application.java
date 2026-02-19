725c111306dba9c2a8fa36bb5be161f90c6839e0
metehansargin <msargin1986@gmail.com>
2026-02-20 01:44:49 +0300

    scheduled annotation added

--- src/main/java/com/metehansargin/springlearn2/Springlearn2Application.java
+++ src/main/java/com/metehansargin/springlearn2/Springlearn2Application.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackages = {"com.metehansargin"})
 @EnableJpaRepositories(basePackages = {"com.metehansargin"})
 @ComponentScan(basePackages = {"com.metehansargin"})
+@EnableScheduling
 @SpringBootApplication
 public class Springlearn2Application {
 
---
+++ src/main/java/com/metehansargin/springlearn2/scheduled/ScheduledExample.java
@@ -0,0 +1,15 @@
+package com.metehansargin.springlearn2.scheduled;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledExample {
+
+    @Scheduled(cron = "*/5 * * * * *")
+    public void write(){
+        for (int i=0;i<10;i++){
+            System.out.println(" "+i);
+        }
+    }
+}
