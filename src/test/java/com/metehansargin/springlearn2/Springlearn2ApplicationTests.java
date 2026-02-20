3db0cf9d6a56b98768eba160cbdfdb8ea5b35f9e
metehansargin <msargin1986@gmail.com>
2026-02-20 13:53:49 +0300

    spring junit test added

--- src/test/java/com/metehansargin/springlearn2/Springlearn2ApplicationTests.java
+++ src/test/java/com/metehansargin/springlearn2/Springlearn2ApplicationTests.java
@@ -1,13 +1,35 @@
 package com.metehansargin.springlearn2;
 
+import com.metehansargin.springlearn2.dto.dtoEmployee;
+import com.metehansargin.springlearn2.service.EmployeeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = {Springlearn2Application.class})
 class Springlearn2ApplicationTests {
 
+	@Autowired
+	private EmployeeService employeeService;
+
+	@BeforeEach
+	void beforeEach(){
+		System.out.println("Burası testlerden once calisan kisim...");
+	}
+
 	@Test
-	void contextLoads() {
+	void testFindByEmployeeId(){
+		dtoEmployee employee=employeeService.getById(4L);
+		assertNotNull(employee);
+	}
+
+	@AfterEach
+	void afterEach(){
+		System.out.println("Burası testlerden sonra calisan kisim...");
 	}
 
 }
