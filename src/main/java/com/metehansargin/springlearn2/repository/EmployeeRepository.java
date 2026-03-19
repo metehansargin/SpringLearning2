27ada2e055a321895b947711c7a627b0d9148481
metehansargin <msargin1986@gmail.com>
2026-03-19 21:49:44 +0300

    authentication entry point class added

--- src/main/java/com/metehansargin/springlearn2/repository/EmployeeRepository.java
+++ src/main/java/com/metehansargin/springlearn2/repository/EmployeeRepository.java
@@ -4,6 +4,9 @@ import com.metehansargin.springlearn2.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+    List<Employee> getEmployeesBy(Employee employee);
 }
